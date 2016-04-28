9605b6cd64d9757db6b7550ecc901a51a88ef520
Aleksey Shornikov <shav89@rambler.ru>
2016-04-28 15:36:29 +0300

    extend gulp tasks

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,48 @@
-"use strict";
-
 function tableInit(){
+	"use strict";
+
+	function dataDownload(offset, limit, callback){
+		var url = "http://ivsevolod.ru/site/getData?offset="+offset+"&limit="+limit;
+	  	$.ajax({
+		  url: url,
+		  dataType: 'json',
+		  success: function(data){
+		  	if(typeof callback === 'function'){
+		  		callback(data);
+		  	}
+		  },
+		  error: function(){
+		  	console.log("Error data load");
+		  }
+		});
+	}
+
+	function getScrollBarWidth () {  
+	    var inner = document.createElement('p');  
+	    inner.style.width = "100%";  
+	    inner.style.height = "200px";  
+	  
+	    var outer = document.createElement('div');  
+	    outer.style.position = "absolute";  
+	    outer.style.top = "0px";  
+	    outer.style.left = "0px";  
+	    outer.style.visibility = "hidden";  
+	    outer.style.width = "200px";  
+	    outer.style.height = "150px";  
+	    outer.style.overflow = "hidden";  
+	    outer.appendChild (inner);  
+	  
+	    document.body.appendChild (outer);  
+	    var w1 = inner.offsetWidth;  
+	    outer.style.overflow = 'scroll';  
+	    var w2 = inner.offsetWidth;  
+	    if (w1 === w2) {w2 = outer.clientWidth;}
+	  
+	    document.body.removeChild (outer);  
+
+	    return (w1 - w2);  
+	}
+
 	var columns =[
 	    {name: 'Column 1'},
 	    {name: 'Column 2'},
@@ -39,48 +81,6 @@ function tableInit(){
 		  	});
 	    }
 	});
-
-	function dataDownload(offset, limit, callback){
-		var url = "http://ivsevolod.ru/site/getData?offset="+offset+"&limit="+limit;
-	  	$.ajax({
-		  url: url,
-		  dataType: 'json',
-		  success: function(data){
-		  	if(typeof callback === 'function'){
-		  		callback(data);
-		  	}
-		  },
-		  error: function(){
-		  	console.log("Error data load");
-		  }
-		});
-	}
-
-	function getScrollBarWidth () {  
-	    var inner = document.createElement('p');  
-	    inner.style.width = "100%";  
-	    inner.style.height = "200px";  
-	  
-	    var outer = document.createElement('div');  
-	    outer.style.position = "absolute";  
-	    outer.style.top = "0px";  
-	    outer.style.left = "0px";  
-	    outer.style.visibility = "hidden";  
-	    outer.style.width = "200px";  
-	    outer.style.height = "150px";  
-	    outer.style.overflow = "hidden";  
-	    outer.appendChild (inner);  
-	  
-	    document.body.appendChild (outer);  
-	    var w1 = inner.offsetWidth;  
-	    outer.style.overflow = 'scroll';  
-	    var w2 = inner.offsetWidth;  
-	    if (w1 == w2) w2 = outer.clientWidth;  
-	  
-	    document.body.removeChild (outer);  
-
-	    return (w1 - w2);  
-	};
 }
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,94 @@
-var gulp = require('gulp');
-var useref = require('gulp-useref');
-var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-clean-css');
-var del = require('del');
+"use strict";
 
-gulp.task('useref', function(){
-  return gulp.src('assets/*.html')
+// Load plugins
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    useref = require('gulp-useref'),
+    gulpIf = require('gulp-if'),
+    cssnano = require('gulp-cssnano'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    browserSync = require('browser-sync'),
+    del = require('del');
+
+// source files paths
+var srcdir = 'assets';
+var src = {
+  html: [srcdir+'/*.html'],
+  css: [srcdir+'/styles/**/*.css'],
+  js: [srcdir+'/scripts/**/*.js'],
+  images: [srcdir+'/images/**']
+}
+
+// distribution files paths
+var distdir = 'dist';
+var dist = {
+  css: distdir + '/styles/',
+  js: distdir + '/scripts/',
+  images: distdir + '/images/',
+  vendor: distdir + '/vendor/',
+  all: distdir + '/**'
+}
+
+// minify app files
+gulp.task('assets', function(){
+  return gulp.src(src.html)
     .pipe(useref())
-    .pipe(gulpif('*.js', uglify()))
-    .pipe(gulpif('*.css', minifyCss()))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest(distdir))
 });
 
-gulp.task('build', ['clean', 'useref'])
+// Images
+gulp.task('images', function() {
+  return gulp.src(src.images)
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest(dist.images))
+});
 
-gulp.task('clean', function(callback) {
-  return del('dist');
+// lint scripts code
+gulp.task('lint', function() {
+    return gulp.src(src.js)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// run server
+ gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: distdir
+    }
+  })
 })
 
+// Clean
+gulp.task('clean', function() {
+  return del(distdir);
+});
 
+// Watch
+gulp.task('watch', ['browserSync'], function() {
+  // Watch app source files
+  gulp.watch([src.html, src.css, src.js], ['assets', 'lint']);
+  gulp.watch(src.js, ['lint']);
+  // Watch image files
+  gulp.watch(src.images, ['images']);
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(dist.all).on('change', browserSync.reload);
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+  gulp.start('assets', 'images', 'lint', 'watch');
+  notify({ message: 'Default task complete' });
+});
+
+// Build task
+gulp.task('build', ['clean', 'assets', 'lint', 'images'], function() {
+  notify({ message: 'Build task complete' })
+});
